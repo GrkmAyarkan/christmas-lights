cd5fa7e0c9ca0e0dc2927cc77475ad0632014108
GrkmAyarkan <grkm.ayarkan@gmail.com>
2026-01-16 17:06:53 +0300

    Add start and stop controls for animation

--- script.js
+++ script.js
@@ -1,4 +1,51 @@
 const lights = document.querySelectorAll(".light");
+const startBtn = document.getElementById("startBtn");
+const stopBtn = document.getElementById("stopBtn");
+
+let currentIndex = 0;
+let intervalId = null;
+const DEFAULT_INTERVAL = 500;
+
+function startAnimation( intervalTime = DEFAULT_INTERVAL) {
+    // Eğer animasyon zaten çalışıyorsa tekrar başlatma
+    if (intervalId !== null) return;
+
+    intervalId = setInterval(() => {
+        // Tüm ışıkları söndür
+        lights.forEach(light => light.classList.remove("active"));
+
+        // Aktif ışığı yak
+        lights[currentIndex].classList.add("active");
+
+        // Bir sonraki index
+        currentIndex++;
+
+        // Sona gelince başa dön
+        if (currentIndex >= lights.length) {
+            currentIndex = 0;
+        }
+    }, intervalTime);
+}
+
+function stopAnimation() {
+    clearInterval(intervalId);
+    intervalId = null;
+
+    //Tüm ışıkları söndür
+    lights.forEach(light => light.classList.remove("active"));
+}
+
+startBtn.addEventListener("click", () => {
+    startAnimation();
+} );
+
+stopBtn.addEventListener("click" , () => {
+    stopAnimation();
+} );
+
+
+
+/* 
 
 let currentIndex = 0;
 
@@ -17,3 +64,5 @@ setInterval(() => {
         currentIndex = 0;
     }
 }, 500);
+
+ */
